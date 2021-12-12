a6972c27cbd633ea59c39f4eea71d6d963ab541c
ToonDubois <60922276+ToonDubois@users.noreply.github.com>
2021-12-12 23:20:12 +0100

    tests complete for SwitchItem

--- src/test/java/com/gildedrose/SwitchItemTest.java
+++ src/test/java/com/gildedrose/SwitchItemTest.java
@@ -1,7 +1,31 @@
 package com.gildedrose;
 
+import com.gildedrose.core.GildedRose;
+import com.gildedrose.core.Item;
+import com.gildedrose.items.AgingItem;
+import com.gildedrose.items.SwitchItem;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SwitchItemTest {
+    private Item[] items = new Item[] {new SwitchItem("Aged Brie", 10, 20)};
+    private Item[] items2 = new Item[] {new SwitchItem("Aged Brie", -4, 20)};
+    private GildedRose app = new GildedRose(items);
+    private GildedRose app2 = new GildedRose(items2);
 
+
+    @Test
+    public void sellInValueKleinerDanNul(){
+        app2.calculateNextDay();
+        assert(app2.items[0].quality == 19);
+    }
+
+    @Test
+    public void sellInValueGroterDanNul(){
+        app.calculateNextDay();
+        assert(app.items[0].quality == 21);
+    }
 }
